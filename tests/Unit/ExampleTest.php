b7825d198685eacd9136a784524dd1f8fb8a2130
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-05-12 19:23:03 +0300

    Инициализировал пакет для разработки под Laravel

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AreaWeb\LaravelPackage\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
